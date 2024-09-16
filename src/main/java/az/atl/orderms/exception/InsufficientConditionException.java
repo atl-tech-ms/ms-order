f3af183def7211d4d460b7958cb019836c92ac12
thehasanovv <hasanov.e95@gmail.com>
2024-09-16 11:04:27 +0400

    InsufficientConditionException added

---
+++ src/main/java/az/atl/orderms/exception/InsufficientConditionException.java
@@ -0,0 +1,7 @@
+package az.atl.orderms.exception;
+
+public class InsufficientConditionException extends RuntimeException{
+    public InsufficientConditionException(String message) {
+        super(message);
+    }
+}
--- src/main/java/az/atl/orderms/service/serviceImpl/OrderServiceImpl.java
+++ src/main/java/az/atl/orderms/service/serviceImpl/OrderServiceImpl.java
@@ -4,8 +4,8 @@ import az.atl.orderms.client.CustomerClient;
 import az.atl.orderms.client.ProductClient;
 import az.atl.orderms.dao.entity.OrderEntity;
 import az.atl.orderms.dao.repository.OrderRepository;
+import az.atl.orderms.exception.InsufficientConditionException;
 import az.atl.orderms.exception.NotFoundException;
-import az.atl.orderms.model.enums.ErrorMessage;
 import az.atl.orderms.model.request.OrderProductDto;
 import az.atl.orderms.model.response.OrderResponseDto;
 import az.atl.orderms.service.OrderService;
@@ -40,10 +40,10 @@ public class OrderServiceImpl implements OrderService {
         var orderEntity = ORDER_MAPPER.buildOrderEntity(dto, totalAmount);
 
         if (product.getCount() < dto.getCount())
-            throw new RuntimeException(INSUFFICIENT_QUANTITY.getMessage());
+            throw new InsufficientConditionException(INSUFFICIENT_QUANTITY.getMessage());
 
         if (customer.getBalance().compareTo(totalAmount) < 0)
-            throw new RuntimeException(INSUFFICIENT_BALANCE.getMessage());
+            throw new InsufficientConditionException(INSUFFICIENT_BALANCE.getMessage());
 
         productClient.reduceProductCount(dto.getProductId(), dto.getCount());
         customerClient.reduceBalance(dto.getCustomerId(), totalAmount);
